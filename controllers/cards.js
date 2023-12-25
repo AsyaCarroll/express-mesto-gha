7960ee7c9d0db40d58d0dade3e73dfdf3207861b
AsyaCarroll <89799056+AsyaCarroll@users.noreply.github.com>
2023-12-25 20:45:31 +0300

    Update cards.js

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const Card = require('../models/card');
-
-// eslint-disable-next-line max-len
 const handleErrorResponse = (res, error, statusCode) => res.status(statusCode).json({ message: error });
 
 const getCards = (req, res) => {
@@ -12,7 +10,6 @@ const getCards = (req, res) => {
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
-
   const ownerId = req.user._id;
 
   Card.create({ name, link, owner: ownerId })
@@ -26,15 +23,11 @@ const createCard = (req, res) => {
     });
 };
 
-// eslint-disable-next-line consistent-return
 const deleteCard = (req, res) => {
   const { cardId } = req.params;
-
-  // Проверка валидности ObjectId
   if (!mongoose.Types.ObjectId.isValid(cardId)) {
     return handleErrorResponse(res, 'Переданы некорректные данные при создании карточки', 400);
   }
-
   Card.findByIdAndDelete(cardId)
     .then((card) => {
       if (!card) {
@@ -46,22 +39,17 @@ const deleteCard = (req, res) => {
     .catch((err) => handleErrorResponse(res, err.error, 500));
 };
 
-// eslint-disable-next-line consistent-return
 const likeCard = async (req, res) => {
   const { cardId } = req.params;
-
-  // Проверка валидности ObjectId
   if (!mongoose.Types.ObjectId.isValid(cardId)) {
     return handleErrorResponse(res, 'Некорректный формат ID карточки', 400);
   }
-
   try {
     const card = await Card.findByIdAndUpdate(
       cardId,
       { $addToSet: { likes: req.user._id } },
       { new: true },
     );
-
     if (!card) {
       handleErrorResponse(res, 'Карточка не найдена', 404);
     } else {
@@ -71,23 +59,17 @@ const likeCard = async (req, res) => {
     handleErrorResponse(res, 'Ошибка на стороне сервера', 500);
   }
 };
-
-// eslint-disable-next-line consistent-return
 const dislikeCard = async (req, res) => {
   const { cardId } = req.params;
-
-  // Проверка валидности ObjectId
   if (!mongoose.Types.ObjectId.isValid(cardId)) {
     return handleErrorResponse(res, 'Некорректный формат ID карточки', 400);
   }
-
   try {
     const card = await Card.findByIdAndUpdate(
       cardId,
       { $pull: { likes: req.user._id } },
       { new: true },
     );
-
     if (!card) {
       handleErrorResponse(res, 'Карточка не найдена', 404);
     } else {
@@ -99,5 +81,9 @@ const dislikeCard = async (req, res) => {
 };
 
 module.exports = {
-  getCards, createCard, deleteCard, likeCard, dislikeCard,
+  getCards, 
+  createCard, 
+  deleteCard, 
+  likeCard, 
+  dislikeCard,
 };
